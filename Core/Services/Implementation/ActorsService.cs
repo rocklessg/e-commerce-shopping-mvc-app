1d012a3bf11e3ee778e4354192a13124c180f896
rocklessg <abdulhafiz.suleiman@decagon.dev>
2021-12-24 13:07:02 +0100

    Re-arranging folder structure

---
+++ Core/Services/Implementation/ActorsService.cs
@@ -0,0 +1,47 @@
+﻿using Core.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Implementation
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var result = await _context.Actors.ToListAsync();
+            return result;
+        }
+
+        public Task<Actor> GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Actor> Update(int id, Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Services/Interface/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Interface
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Task<Actor> GetById();
+        void Add(Actor actor);
+        Task<Actor> Update(int id, Actor actor);
+        void Delete(int id);
+    }
+}
