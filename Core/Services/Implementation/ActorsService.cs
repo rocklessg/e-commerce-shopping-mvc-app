ab63c4f994f35c197ecaef0831ae30706f820458
rocklessg <abdulhafiz.suleiman@decagon.dev>
2021-12-27 23:02:46 +0100

    Generic repository setup completed

--- Core/Services/Implementation/ActorsService.cs
+++ Core/Services/Implementation/ActorsService.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Services.Interface;
 using Microsoft.EntityFrameworkCore;
 using Models;
+using Models.Base;
 using Models.Data;
 using System;
 using System.Collections.Generic;
@@ -10,44 +11,8 @@ using System.Threading.Tasks;
 
 namespace Core.Services.Implementation
 {
-    public class ActorsService : IActorsService
+    public class ActorsService : EntityBaseRepository<Actor>, IActorsService
     {
-        private readonly AppDbContext _context;
-
-        public ActorsService(AppDbContext context)
-        {
-            _context = context;
-        }
-        public async Task AddAsync(Actor actor)
-        {
-            await _context.Actors.AddAsync(actor);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            _context.Actors.Remove(result);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var result = await _context.Actors.ToListAsync();
-            return result;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            return result;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
-        {
-            _context.Update(newActor);
-            await _context.SaveChangesAsync();
-            return newActor;
-        }
+        public ActorsService(AppDbContext context) : base(context) { }
     }
 }
---
+++ Models/Base/IEntityBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Base
+{
+    /// <summary>
+    /// Generic repository interface
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(int id, T entity);
+        Task DeleteAsync(int id);
+    }
+}
